f9330f2c1f7447c712c42c52d38259e28c27ae51
Sridhar Ratnakumar <srid@srid.ca>
2021-02-23 23:11:21 -0500

    make readonly to prevent accidental edit

--- bin/privatezk.sh
+++ bin/privatezk.sh
@@ -8,10 +8,12 @@ shopt -s globasciiranges
 ZK=$HOME/Documents/zk
 
 #TODO: This needs to be perfected
+chmod -R u+w Imported-From-Private-ZK/
 rm -rf ./Imported-From-Private-ZK/*
 
 (cd $ZK && nix-shell -p ripgrep --run "rg -lU \"^---(\r)*\nslug: \" .") > /tmp/zkimport
 rsync -zarv --files-from=/tmp/zkimport  ${ZK} ./Imported-From-Private-ZK/
+chmod -R a-w Imported-From-Private-ZK/
 git add Imported-From-Private-ZK
 git status
 echo "Run: git diff --staged"
